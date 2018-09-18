27895fee5fee74aa8c1d4fe5d9a3d5ef8d06f5d3
knopers8 <piotr.jan.konopka@cern.ch>
2018-09-18 09:12:43 +0200

    A training ground workflow for timer inputs

---
+++ Framework/TestWorkflows/src/timersTest.cxx
@@ -0,0 +1,169 @@
+// Copyright CERN and copyright holders of ALICE O2. This software is
+// distributed under the terms of the GNU General Public License v3 (GPL
+// Version 3), copied verbatim in the file "COPYING".
+//
+// See http://alice-o2.web.cern.ch/license for full licensing information.
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+/// The workflow consists of two data producers, accumulator and printer. Accumulator wants to receive data from two
+/// producers at the same time and send some output data each x seconds. Printer shows the result sent by accumulator.
+
+
+#include "Framework/CompletionPolicy.h"
+#include "Framework/CompletionPolicyHelpers.h"
+#include "Framework/DeviceSpec.h"
+#include "Headers/DataHeader.h"
+
+using namespace o2::framework;
+using namespace o2::header;
+
+void customize(std::vector<CompletionPolicy>& policies)
+{
+  CompletionPolicy timerAndData{
+    "timerAndData",
+    [](DeviceSpec const& dev) -> bool { return dev.name == "accumulator"; },
+    [](gsl::span<PartRef const> const& inputs) -> CompletionPolicy::CompletionOp {
+
+      bool timerPresent = false;
+      size_t dataInputsPresent = 0;
+      for (auto& input : inputs) {
+        if (input.header == nullptr || input.payload == nullptr) {
+          continue;
+        }
+
+        const DataHeader* inputHeader = get<DataHeader*>(input.header->GetData());
+        if (!strncmp(inputHeader->dataDescription.str, "TIMER", 5)) {
+          timerPresent = true;
+        } else {
+          dataInputsPresent++;
+        }
+      }
+
+      if (dataInputsPresent == inputs.size() - 1) {
+        // both data inputs present and maybe timer => consume data, execute periodic procedure if timer is present
+        return CompletionPolicy::CompletionOp::Consume;
+      } else if (timerPresent) {
+        // only timer and possibly some partial data => execute periodic procedure, wait for the rest of data
+        return CompletionPolicy::CompletionOp::Process;
+      } else {
+        // partial data => wait for the rest
+        return CompletionPolicy::CompletionOp::Wait;
+      }
+    }
+  };
+
+  policies.push_back(timerAndData);
+}
+
+#include "Framework/runDataProcessing.h"
+#include <chrono>
+
+using namespace std::chrono;
+
+WorkflowSpec defineDataProcessing(ConfigContext const&)
+{
+
+  DataProcessorSpec producer1{
+    "producer1",
+    Inputs{},
+    Outputs{
+      {{ "output1" }, "TST", "DATA1" }
+    },
+    AlgorithmSpec{
+      (AlgorithmSpec::InitCallback) [](InitContext& ic) {
+        srand(getpid());
+        return (AlgorithmSpec::ProcessCallback) [](ProcessingContext& ctx) {
+          sleep(rand() % 2);
+          auto data1 = ctx.outputs().make<int>(OutputRef{ "output1" }, 1);
+          data1[0] = 1;
+        };
+      }
+    }
+  };
+
+  DataProcessorSpec producer2{
+    "producer2",
+    Inputs{},
+    Outputs{
+      {{ "output2" }, "TST", "DATA2" }
+    },
+    AlgorithmSpec{
+      (AlgorithmSpec::InitCallback) [](InitContext& ic) {
+        srand(getpid());
+        return (AlgorithmSpec::ProcessCallback) [](ProcessingContext& ctx) {
+          sleep(rand() % 2);
+          auto data2 = ctx.outputs().make<int>(OutputRef{ "output2" }, 1);
+          data2[0] = 1;
+        };
+      }
+    }
+  };
+
+  DataProcessorSpec accumulator{
+    "accumulator",
+    Inputs{
+      { "data1", "TST", "DATA1" },
+      { "data2", "TST", "DATA2" },
+      { "timer", "TST", "TIMER", 0, Lifetime::Timer }
+    },
+    Outputs{
+      {{ "sum" }, "TST", "SUM" }
+    },
+    AlgorithmSpec{
+      (AlgorithmSpec::InitCallback) [](InitContext& ictx) {
+
+        std::shared_ptr<int> sum = std::make_shared<int>(0);
+        return (AlgorithmSpec::ProcessCallback) [sum](ProcessingContext& ctx) {
+
+          if (ctx.inputs().isValid("data1") && ctx.inputs().isValid("data2")) {
+            LOG(INFO) << "data input";
+            *sum += ctx.inputs().get<int>("data1");
+            *sum += ctx.inputs().get<int>("data2");
+          }
+
+          if (ctx.inputs().isValid("timer")) {
+            LOG(INFO) << "timer input";
+            ctx.outputs().snapshot<int>(Output{ "TST", "SUM" }, *sum);
+          }
+
+          LOG(INFO) << "sum: " << *sum;
+        };
+      }
+    },
+    Options{
+      ConfigParamSpec{ "period-TST-TIMER", VariantType::Int, 5000, { "timer period" }}
+    }
+  };
+
+  DataProcessorSpec printer{
+    "printer",
+    Inputs{
+      { "sum", "TST", "SUM" }
+    },
+    Outputs{},
+    AlgorithmSpec{
+      (AlgorithmSpec::InitCallback) [](InitContext& ictx) {
+
+        auto time = std::make_shared<steady_clock::time_point>(steady_clock::now());
+        return (AlgorithmSpec::ProcessCallback) [time](ProcessingContext& ctx) {
+          auto timeNow = steady_clock::now();
+          LOG(INFO) << "The sum is: " << ctx.inputs().get<int>("sum")
+                    << ", time interval of " << duration_cast<milliseconds>(timeNow - *time).count() / 1000.0 << "s";
+          *time = timeNow;
+        };
+      }
+    }
+  };
+
+  WorkflowSpec specs{
+    producer1,
+    producer2,
+    accumulator,
+    printer
+  };
+
+  return specs;
+}
