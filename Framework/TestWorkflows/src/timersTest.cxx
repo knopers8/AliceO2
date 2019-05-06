e92cc16988c878a6b0d026b32502d61c86af2865
Piotr Konopka <piotr.jan.konopka@cern.ch>
2019-05-06 15:48:59 +0200

    using latest api for timer playground
    the expected behaviour was still not achieved. sometimes data inputs are
    asigned to the same timeslot as timer, which results in dropping data.

--- Framework/TestWorkflows/src/timersTest.cxx
+++ Framework/TestWorkflows/src/timersTest.cxx
@@ -11,7 +11,6 @@
 /// The workflow consists of two data producers, accumulator and printer. Accumulator wants to receive data from two
 /// producers at the same time and send some output data each x seconds. Printer shows the result sent by accumulator.
 
-
 #include "Framework/CompletionPolicy.h"
 #include "Framework/CompletionPolicyHelpers.h"
 #include "Framework/DeviceSpec.h"
@@ -27,35 +26,42 @@ void customize(std::vector<CompletionPolicy>& policies)
     [](DeviceSpec const& dev) -> bool { return dev.name == "accumulator"; },
     [](gsl::span<PartRef const> const& inputs) -> CompletionPolicy::CompletionOp {
 
-      bool timerPresent = false;
+      // fixme: we assume that there is one timer input and the rest are data inputs. If some other implicit inputs are
+      //  added, this will break.
+      size_t dataInputsExpected = inputs.size() - 1;
       size_t dataInputsPresent = 0;
+
+      // todo: return the action directly, when the errors are fixed
+      CompletionPolicy::CompletionOp action = CompletionPolicy::CompletionOp::Wait;
+
       for (auto& input : inputs) {
         if (input.header == nullptr || input.payload == nullptr) {
           continue;
         }
 
-        const DataHeader* inputHeader = get<DataHeader*>(input.header->GetData());
-        assert(inputHeader);
-        if (!strncmp(inputHeader->dataDescription.str, "TIMER", 5)) {
-          timerPresent = true;
+        const auto* dataHeader = get<DataHeader*>(input.header.get()->GetData());
+        assert(dataHeader);
+
+        if (!strncmp(dataHeader->dataDescription.str, "TIMER", 5)) {
+          action = CompletionPolicy::CompletionOp::Consume;
         } else {
           dataInputsPresent++;
         }
       }
 
-      if (dataInputsPresent == inputs.size() - 1) {
-        // both data inputs present and maybe timer => consume data, execute periodic procedure if timer is present
-        return CompletionPolicy::CompletionOp::Consume;
-      } else if (timerPresent) {
-//         only timer and no data => execute periodic procedure, wait for the rest of data
-        if (dataInputsPresent) {
-          LOG(WARN) << "DATA INPUTS PRESENT IN THIS TIMESLICE, LOSING DATA PROBABLY";
+      if (dataInputsPresent && action == CompletionPolicy::CompletionOp::Consume) {
+        if (dataInputsPresent == dataInputsExpected) {
+          LOG(WARN) << "TIMER AND COMPLETE DATA IN THE SAME TIMESLOT";
+        } else {
+          LOG(ERROR) << "TIMER AND INCOMPLETE DATA IN THE SAME TIMESLOT";
         }
-        return CompletionPolicy::CompletionOp::Process;
-      } else {
-        // partial data => wait for the rest
-        return CompletionPolicy::CompletionOp::Wait;
       }
+
+      if (dataInputsPresent == dataInputsExpected) {
+        action = CompletionPolicy::CompletionOp::Consume;
+      }
+
+      return action;
     }
   };
 
@@ -114,9 +120,9 @@ WorkflowSpec defineDataProcessing(ConfigContext const&)
   DataProcessorSpec accumulator{
     "accumulator",
     Inputs{
-      { "data1", "TST", "DATA1" },
-      { "data2", "TST", "DATA2" },
-      { "timer", "TST", "TIMER", 0, Lifetime::Timer }
+      { "data1",     "TST", "DATA1" },
+      { "data2",     "TST", "DATA2" },
+      { "timer-acc", "TST", "TIMER", 0, Lifetime::Timer }
     },
     Outputs{
       {{ "sum" }, "TST", "SUM" }
@@ -129,28 +135,50 @@ WorkflowSpec defineDataProcessing(ConfigContext const&)
         // note that some data will be lost anyway, when timeslice	is booked on existing partial data. we need some
         // mechanism for preventing that - either separate 'cache' for timers or timesliceIndex API call for overbooking
         // only the obsolete timeslices
+        // edit: it seems that messages with data are actually matched to timeslots with timers, which should not happen.
         auto bookTimesliceCallback = [&timesliceIndex = ictx.services().get<TimesliceIndex>(),
           time = std::make_shared<steady_clock::time_point>(steady_clock::now())]() {
+
           auto timeNow = steady_clock::now();
-          static TimesliceId i{0};
-          if (duration_cast<milliseconds>(timeNow - *time).count() > 5000 ) {
-            timesliceIndex.bookTimeslice(i);
+          static TimesliceId i{ 0 };
+
+          if (duration_cast<milliseconds>(timeNow - *time).count() > 5000) {
+            data_matcher::VariableContext context;
+            context.put(data_matcher::ContextUpdate{ 0, i.value });
+//            context.put(data_matcher::ContextUpdate{0, static_cast<uint64_t>(steady_clock::now().time_since_epoch().count())});
+            context.commit();
+
+            timesliceIndex.replaceLRUWith(context);
+
             i.value++;
-//            LOG(INFO) << "i.value " << i.value << ", timeNow " << duration_cast<milliseconds>(steady_clock::now().time_since_epoch()).count();
             *time = timeNow;
+            //            LOG(INFO) << "i.value " << i.value << ", timeNow " << duration_cast<milliseconds>(steady_clock::now().time_since_epoch()).count();
           }
         };
 
         ictx.services().get<CallbackService>().set(CallbackService::Id::ClockTick, bookTimesliceCallback);
+//        ictx.services().get<CallbackService>().set(CallbackService::Id::Idle, bookTimesliceCallback);
         return (AlgorithmSpec::ProcessCallback) [sum](ProcessingContext& ctx) {
 
           if (ctx.inputs().isValid("data1") && ctx.inputs().isValid("data2")) {
-            LOG(INFO) << "data input: " << ctx.inputs().get<int>("data1") << " " << ctx.inputs().get<int>("data2");
-            *sum += ctx.inputs().get<int>("data1");
-            *sum += ctx.inputs().get<int>("data2");
+            static int data1 = -1;
+            if (data1 + 1 != ctx.inputs().get<int>("data1")) {
+              LOG(ERROR) << "SOMETHING WAS LOST";
+            }
+            data1 = ctx.inputs().get<int>("data1");
+            LOG(INFO) << "data1 input: " << data1;
+            *sum += data1;
+
+            static int data2 = -1;
+            if (data2 + 1 != ctx.inputs().get<int>("data2")) {
+              LOG(ERROR) << "SOMETHING WAS LOST";
+            }
+            data2 = ctx.inputs().get<int>("data2");
+            LOG(INFO) << "data2 input: " << data2;
+            *sum += data2;
           }
 
-          if (ctx.inputs().isValid("timer")) {
+          if (ctx.inputs().isValid("timer-acc")) {
             LOG(INFO) << "timer input";
             ctx.outputs().snapshot<int>(Output{ "TST", "SUM" }, *sum);
           }
@@ -160,7 +188,7 @@ WorkflowSpec defineDataProcessing(ConfigContext const&)
       }
     },
     Options{
-      ConfigParamSpec{ "period-TST-TIMER", VariantType::Int, 5000, { "timer period" }}
+      ConfigParamSpec{ "period-timer-acc", VariantType::Int, 5000000, { "timer period" }}
     }
   };
 
@@ -196,7 +224,7 @@ WorkflowSpec defineDataProcessing(ConfigContext const&)
         return (AlgorithmSpec::ProcessCallback) [](ProcessingContext& ctx) {
 
           LOG(INFO) << "=== sink ===";
-          if (ctx.inputs().isValid("data1") ){
+          if (ctx.inputs().isValid("data1")) {
             auto d = ctx.inputs().get("data1");
             const auto* dpHeader = get<DataProcessingHeader*>(d.header);
             assert(dpHeader);
@@ -212,7 +240,6 @@ WorkflowSpec defineDataProcessing(ConfigContext const&)
       }
     }
   };
-
 
 
   WorkflowSpec specs{
