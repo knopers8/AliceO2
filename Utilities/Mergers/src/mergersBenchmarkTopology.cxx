10e2c9fc0cb90920fd077737254aacdc6103dcc8
Piotr Konopka <piotr.jan.konopka@cern.ch>
2019-10-18 15:04:34 +0200

    don't show full histogram contents if histo_bins > 100

--- Utilities/Mergers/src/mergersBenchmarkTopology.cxx
+++ Utilities/Mergers/src/mergersBenchmarkTopology.cxx
@@ -133,6 +133,10 @@ WorkflowSpec defineDataProcessing(ConfigContext const& config)
             std::string bins = "BINS:";
             for (int i = 1; i <= histo->GetNbinsX(); i++) {
               bins += " " + std::to_string((int) histo->GetBinContent(i));
+              if (i >= 100) {
+                LOG(INFO) << "Trimming the output to 100 entries, total is: " << histo->GetNbinsX();
+                break;
+              }
             }
             LOG(INFO) << bins;
           };
