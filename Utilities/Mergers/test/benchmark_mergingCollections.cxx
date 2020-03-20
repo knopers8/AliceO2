c2bef78231bc2e422ab575ae7a0fe8e55ef6ca05
Piotr Jan Konopka <piotr.jan.konopka@cern.ch>
2020-03-20 16:30:14 +0100

    add ttree test, use integer histograms

--- Utilities/Mergers/test/benchmark_mergingCollections.cxx
+++ Utilities/Mergers/test/benchmark_mergingCollections.cxx
@@ -28,28 +28,9 @@ namespace bh = boost::histogram;
 
 #include <ctime>
 
-#define BENCHMARK_RANGE_COLLECTIONS Arg(1)->Arg(1 << 2)->Arg(1 << 4)->Arg(1 << 6)->Arg(1 << 8)->Arg(1 << 10)->Arg(1 << 12)
 
-// A simple test where an input is provided
-// and the subsequent InputRecord is immediately requested.
-static void BM_RelayMessageCreation(benchmark::State& state)
+static void BM_mergingCollectionsTH1I(benchmark::State& state)
 {
-//  Monitoring metrics;
-
-  for (auto _ : state) {
-    //state.PauseTiming();
-
-    // do stuff here
-
-    //state.ResumeTiming();
-  }
-}
-
-//BENCHMARK(BM_RelayMessageCreation);
-
-static void BM_mergingCollectionsTH1F(benchmark::State& state)
-{
-
   size_t collectionSize = state.range(0);
   size_t bins = 62500; // makes 250kB
 
@@ -57,12 +38,12 @@ static void BM_mergingCollectionsTH1F(benchmark::State& state)
   collection->SetOwner(true);
   TF1* uni = new TF1("uni", "1", 0, 1000000);
   for (size_t i = 0; i < collectionSize; i++) {
-    TH1F* h = new TH1F(("test" + std::to_string(i)).c_str(), "test", bins, 0, 1000000);
+    TH1I* h = new TH1I(("test" + std::to_string(i)).c_str(), "test", bins, 0, 1000000);
     h->FillRandom("uni", 50000);
     collection->Add(h);
   }
 
-  TH1F* m = new TH1F("merged", "merged", bins, 0, 1000000);
+  TH1I* m = new TH1I("merged", "merged", bins, 0, 1000000);
 
   for (auto _ : state) {
     m->Merge(collection, "-NOCHECK");
@@ -72,9 +53,8 @@ static void BM_mergingCollectionsTH1F(benchmark::State& state)
   delete m;
   delete uni;
 }
-BENCHMARK(BM_mergingCollectionsTH1F)->BENCHMARK_RANGE_COLLECTIONS;
 
-static void BM_mergingCollectionsTH2F(benchmark::State& state)
+static void BM_mergingCollectionsTH2I(benchmark::State& state)
 {
 
   size_t collectionSize = state.range(0);
@@ -84,12 +64,12 @@ static void BM_mergingCollectionsTH2F(benchmark::State& state)
   collection->SetOwner(true);
   TF2* uni = new TF2("uni", "1", 0, 1000000, 0, 1000000);
   for (size_t i = 0; i < collectionSize; i++) {
-    TH2F* h = new TH2F(("test" + std::to_string(i)).c_str(), "test", bins, 0, 1000000, bins, 0, 1000000);
+    TH2I* h = new TH2I(("test" + std::to_string(i)).c_str(), "test", bins, 0, 1000000, bins, 0, 1000000);
     h->FillRandom("uni", 50000);
     collection->Add(h);
   }
 
-  TH2F* m = new TH2F("merged", "merged", bins, 0, 1000000, bins, 0, 1000000);
+  TH2I* m = new TH2I("merged", "merged", bins, 0, 1000000, bins, 0, 1000000);
 
   for (auto _ : state) {
     m->Merge(collection, "-NOCHECK");
@@ -98,18 +78,17 @@ static void BM_mergingCollectionsTH2F(benchmark::State& state)
   delete collection;
   delete m;
 }
-//BENCHMARK(BM_mergingCollectionsTH2F)->BENCHMARK_RANGE_COLLECTIONS;
 
-static void BM_mergingCollectionsTH3F(benchmark::State& state)
+static void BM_mergingCollectionsTH3I(benchmark::State& state)
 {
   size_t collectionSize = state.range(0);
-  size_t bins = 40; // 250 bins * 250 bins * 250 bins * 4B makes 256kB
+  size_t bins = 40; // 40 bins * 40 bins * 40 bins * 4B makes 256kB
 
   TCollection* collection = new TObjArray();
   collection->SetOwner(true);
   TF3* uni = new TF3("uni", "1", 0, 1000000, 0, 1000000, 0, 1000000);
   for (size_t i = 0; i < collectionSize; i++) {
-    TH3F* h = new TH3F(("test" + std::to_string(i)).c_str(), "test",
+    TH3I* h = new TH3I(("test" + std::to_string(i)).c_str(), "test",
                        bins, 0, 1000000,
                        bins, 0, 1000000,
                        bins, 0, 1000000);
@@ -117,7 +96,7 @@ static void BM_mergingCollectionsTH3F(benchmark::State& state)
     collection->Add(h);
   }
 
-  TH3F* m = new TH3F("merged", "merged", bins, 0, 1000000, bins, 0, 1000000, bins, 0, 1000000);
+  TH3I* m = new TH3I("merged", "merged", bins, 0, 1000000, bins, 0, 1000000, bins, 0, 1000000);
 
   for (auto _ : state) {
     m->Merge(collection, "-NOCHECK");
@@ -126,7 +105,6 @@ static void BM_mergingCollectionsTH3F(benchmark::State& state)
   delete collection;
   delete m;
 }
-//BENCHMARK(BM_mergingCollectionsTH3F)->BENCHMARK_RANGE_COLLECTIONS;
 
 static void BM_mergingCollectionsTHNSparse(benchmark::State& state)
 {
@@ -135,7 +113,7 @@ static void BM_mergingCollectionsTHNSparse(benchmark::State& state)
   const Double_t min = 0.0;
   const Double_t max = 1000000.0;
   const size_t dim = 10;
-  const Int_t bins = 250; // 250 bins * 250 bins * 250 bins * 4B makes 256kB
+  const Int_t bins = 250;
   const Int_t binsDims[dim] = {bins, bins, bins, bins, bins, bins, bins, bins, bins, bins};
   const Double_t mins[dim] = { min, min, min, min, min, min, min, min, min, min};
   const Double_t maxs[dim] = { max, max, max, max, max, max, max, max, max, max};
@@ -151,7 +129,7 @@ static void BM_mergingCollectionsTHNSparse(benchmark::State& state)
     collection->SetOwner(true);
     for (size_t i = 0; i < collectionSize; i++) {
 
-      auto* h = new THnSparseF(("test" + std::to_string(i)).c_str(), "test", dim, binsDims, mins, maxs);
+      auto* h = new THnSparseI(("test" + std::to_string(i)).c_str(), "test", dim, binsDims, mins, maxs);
       for (size_t entry = 0; entry < 50000; entry++) {
         gen.RndmArray(dim, randomArray);
         for (double r : randomArray) {
@@ -161,7 +139,7 @@ static void BM_mergingCollectionsTHNSparse(benchmark::State& state)
       }
       collection->Add(h);
     }
-    auto* m = new THnSparseF("merged", "merged", dim, binsDims, mins, maxs);
+    auto* m = new THnSparseI("merged", "merged", dim, binsDims, mins, maxs);
 
     state.ResumeTiming();
     m->Merge(collection);
@@ -172,7 +150,56 @@ static void BM_mergingCollectionsTHNSparse(benchmark::State& state)
     delete m;
   }
 }
-//BENCHMARK(BM_mergingCollectionsTHNSparse)->BENCHMARK_RANGE_COLLECTIONS;
+
+static void BM_mergingCollectionsTTree(benchmark::State& state)
+{
+  size_t collectionSize = state.range(0);
+
+  struct format1 {
+    Int_t a;
+    Long64_t b;
+    Float_t c;
+    Double_t d;
+  } branch1;
+  ULong64_t branch2;
+
+  auto createTree = [&](std::string name) -> TTree* {
+    TTree* tree = new TTree();
+    tree->SetName(name.c_str());
+    tree->Branch("b1", &branch1, "a/I:b/L:c/F:d/D");
+    tree->Branch("b2", &branch2);
+    return tree;
+  };
+
+  TRandomMT64 gen;
+  gen.SetSeed(std::time(nullptr));
+  Double_t randomArray[5];
+
+  for (auto _ : state) {
+    state.PauseTiming();
+    TCollection* collection = new TObjArray();
+    collection->SetOwner(true);
+    for (size_t i = 0; i < collectionSize; i++) {
+      TTree* t = createTree(std::to_string(i));
+      for (size_t entry = 0; entry < 50000; entry++) {
+        gen.RndmArray(5, randomArray);
+        branch1 = {static_cast<Int_t>(randomArray[0]), static_cast<Long64_t>(randomArray[1]), static_cast<Float_t>(randomArray[2]), randomArray[3]};
+        branch2 = randomArray[4];
+        t->Fill();
+      }
+      collection->Add(t);
+    }
+    TTree* merged = createTree("merged");
+
+    state.ResumeTiming();
+    merged->Merge(collection);
+
+    state.PauseTiming();
+
+    delete collection;
+    delete merged;
+  }
+}
 
 static void BM_mergingPODCollections(benchmark::State& state)
 {
@@ -220,9 +247,8 @@ static void BM_mergingPODCollections(benchmark::State& state)
   delete m;
   delete uni;
 }
-BENCHMARK(BM_mergingPODCollections)->BENCHMARK_RANGE_COLLECTIONS;
 
-static void BM_mergingBoostCollections(benchmark::State& state)
+static void BM_mergingBoostRegular1DCollections(benchmark::State& state)
 {
   const double min = 0.0;
   const double max = 1000000.0;
@@ -240,7 +266,6 @@ static void BM_mergingBoostCollections(benchmark::State& state)
   Double_t randomArray[randoms];
 
   for (size_t i = 0; i < collectionSize; i++) {
-    //    auto* v = new std::vector<float>(bins, 0);
     collection.emplace_back(std::move(bh::make_histogram(bh::axis::regular<>(bins, min, max, "x"))));
 
     auto& h = collection.back();
@@ -264,6 +289,72 @@ static void BM_mergingBoostCollections(benchmark::State& state)
 
   delete uni;
 }
-BENCHMARK(BM_mergingBoostCollections)->BENCHMARK_RANGE_COLLECTIONS;
+
+static void BM_mergingBoostRegular2DCollections(benchmark::State& state)
+{
+  const double min = 0.0;
+  const double max = 1000000.0;
+  const size_t collectionSize = state.range(0);
+  const size_t bins = 250; // 250 bins * 250 bins * 4B makes 250kB
+
+  auto merged = bh::make_histogram(bh::axis::regular<>(bins, min, max, "x"), bh::axis::regular<>(bins, min, max, "y"));
+
+  std::vector<decltype(merged)> collection;
+  TF1* uni = new TF1("uni", "1", 0, 1000000);
+
+  const size_t randoms = 50000;
+  TRandomMT64 gen;
+  gen.SetSeed(std::time(nullptr));
+  Double_t randomArrayX[randoms];
+  Double_t randomArrayY[randoms];
+
+  for (size_t i = 0; i < collectionSize; i++) {
+    collection.emplace_back(std::move(bh::make_histogram(bh::axis::regular<>(bins, min, max, "x"), bh::axis::regular<>(bins, min, max, "y"))));
+
+    auto& h = collection.back();
+    static_assert(std::is_reference<decltype(h)>::value);
+
+    gen.RndmArray(randoms, randomArrayX);
+    gen.RndmArray(randoms, randomArrayY);
+    for (size_t r = 0; r < randoms; r++) {
+      h(randomArrayX[r] * max, randomArrayY[r] * max);
+    }
+  }
+
+  auto merge = [&](size_t i) {
+    merged += collection[i];
+  };
+
+  for (auto _ : state) {
+    for (size_t i = 0; i < collectionSize; i++) {
+      merge(i);
+    }
+  }
+
+  delete uni;
+}
+
+// one by one comparison
+//BENCHMARK(BM_mergingCollectionsTH1I)->Arg(1);
+//BENCHMARK(BM_mergingCollectionsTH2I)->Arg(1);
+//BENCHMARK(BM_mergingCollectionsTH3I)->Arg(1);
+//BENCHMARK(BM_mergingCollectionsTHNSparse)->Arg(1);
+//BENCHMARK(BM_mergingPODCollections)->Arg(1);
+//BENCHMARK(BM_mergingBoostRegular1DCollections)->Arg(1);
+//BENCHMARK(BM_mergingBoostRegular2DCollections)->Arg(1);
+BENCHMARK(BM_mergingCollectionsTTree)->Arg(1);
+
+// collections
+#define BENCHMARK_RANGE_COLLECTIONS Arg(1)->Arg(1 << 2)->Arg(1 << 4)->Arg(1 << 6)->Arg(1 << 8)
+// ->Arg(1 << 10)->Arg(1 << 12)
+
+//BENCHMARK(BM_mergingCollectionsTH1I)->BENCHMARK_RANGE_COLLECTIONS;
+//BENCHMARK(BM_mergingCollectionsTH2I)->BENCHMARK_RANGE_COLLECTIONS;
+//BENCHMARK(BM_mergingCollectionsTH3I)->BENCHMARK_RANGE_COLLECTIONS;
+//BENCHMARK(BM_mergingCollectionsTHNSparse)->BENCHMARK_RANGE_COLLECTIONS;
+//BENCHMARK(BM_mergingPODCollections)->BENCHMARK_RANGE_COLLECTIONS;
+//BENCHMARK(BM_mergingBoostRegular1DCollections)->BENCHMARK_RANGE_COLLECTIONS;
+//BENCHMARK(BM_mergingBoostRegular2DCollections)->BENCHMARK_RANGE_COLLECTIONS;
+BENCHMARK(BM_mergingCollectionsTTree)->BENCHMARK_RANGE_COLLECTIONS;
 
 BENCHMARK_MAIN();
